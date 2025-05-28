9f630c1dcd4fb08154fd5e2380dd97980e1aee69
W <986083475@qq.com>
2025-05-28 14:19:00 +0800

    fixed InvisLevitation.

--- scriptRecipes/InvisLevitation-60m-1201s.js
+++ scriptRecipes/InvisLevitation-60m-1201s.js
@@ -45,10 +45,10 @@ function beta() {
   logAddHeatVortex(2.5);
   heatAndPourToEdge(1, 10);
   derotateToAngle(-113);
-  logAddHeatVortex(0.886); // Empirical
+  logAddHeatVortex(0.788); // Empirical
   stirIntoVortex();
   heatAndPourToEdge(0.3, 7);
-  logAddHeatVortex(3.95); //Empirical
+  logAddHeatVortex(3.98); //Empirical
   stirIntoVortex();
   heatAndPourToEdge(0.3, 17); // heavier continuous pour&stir save 1 more moon.
   logAddHeatVortex(0.15);
@@ -60,10 +60,10 @@ function beta() {
   logAddHeatVortex(5.7);
   pourToEdge();
   heatAndPourToEdge(1, 9);
-  logAddHeatVortex(1.81); // Empirical
+  logAddHeatVortex(1.75); // Empirical
   logAddStirCauldron(7.1); // roughly
   stirToTier(10.46, 35.57, 0.0);
-  logAddPourSolvent(5.3); // Empirical
+  logAddPourSolvent(5.35); // Empirical
   let returnSalt = 280; // Empirical. Add less is not necessarily better.
   logAddSunSalt(returnSalt);
   stirToDangerZoneExit();
@@ -71,14 +71,14 @@ function beta() {
   stirToDangerZoneExit();
   logAddPourSolvent(1.36); // Empirical
   stirIntoVortex();
-  logAddHeatVortex(9.28); // Empirical
+  logAddHeatVortex(9.1); // Empirical
   pourIntoVortex(8, 16);
   logAddHeatVortex(Infinity);
   console.log(Math.ceil((180 + currentPlot.pendingPoints[0].angle) / 0.36) + 280);
   // predict 2nd part of sun salt
-  straighten(Infinity, degToRad(50), "sun", 105);
+  straighten(Infinity, degToRad(48), "sun", 104);
   // pass the skeleton
-  const saltToReverse = Math.ceil((180 + currentPlot.pendingPoints[0].angle) / 0.36);
+  const saltToReverse = Math.ceil((180.01 + currentPlot.pendingPoints[0].angle) / 0.36);
   // match the angle entering vortex.
   straighten(Infinity, degToRad(-65.9), "sun", saltToReverse);
   stirIntoVortex();
@@ -86,14 +86,12 @@ function beta() {
   logAddHeatVortex(4.6);
   pourToEdge();
   heatAndPourToEdge(1, 9);
-  logAddHeatVortex(5.4);
-  derotateToAngle(saltToDeg("moon", 103 + 33));
-  // -25 is not the proper straightening angle meeting optimal condition.
-  // More test is needed.
-  logAddStirCauldron(9.8);
-  straighten(Infinity, degToRad(-25), "sun", 102);
+  logAddHeatVortex(5.12);
+  derotateToAngle(saltToDeg("moon", 100 + 33.3));
+  logAddStirCauldron(9.5);
+  straighten(Infinity, degToRad(5), "sun", 99);
   // last salt for precise centering
-  logAddStirCauldron(1.83);
+  logAddStirCauldron(0.736);
   logAddSunSalt(1);
   logAddStirCauldron(3);
   console.log("path deviation: " + stirToNearestTarget(-4.22, 36.37));
