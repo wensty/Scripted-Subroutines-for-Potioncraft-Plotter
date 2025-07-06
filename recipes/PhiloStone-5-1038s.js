6e2082bc4b6a5fcd47c96daa776c215d052d622c
W <986083475@qq.com>
2025-07-06 09:15:54 +0800

    Updated Philo-Stone 5.

--- recipes/PhiloStone-5-1038s.js
+++ recipes/PhiloStone-5-1038s.js
@@ -16,7 +16,7 @@ import {
   checkBase,
   straighten,
 } from "../main";
-import { SaltAngle, DeviationT1, EntityPotionEffect, Salt, Effects } from "../main";
+import { DeviationT1, Entity, Salt, Effects } from "../main";
 
 import { currentPlot } from "@potionous/plot";
 
@@ -24,7 +24,7 @@ function beta() {
   checkBase("water");
   logAddSunSalt(166);
   logSkirt();
-  derotateToAngle(10.03, { toAngle: false });
+  derotateToAngle(10.04, { toAngle: false });
   straighten(degToRad(41.5), Salt.Sun, { preStirLength: 5.0, maxGrains: 187 });
   logSkirt();
   stirToTier(Effects.Water.Mana, {
@@ -32,30 +32,32 @@ function beta() {
     maxDeviation: DeviationT1,
     ignoreAngle: true,
   });
-  console.log(radToDeg(Math.PI + getBottlePolarAngleByEntity(EntityPotionEffect)));
+  console.log(radToDeg(Math.PI + getBottlePolarAngleByEntity(Entity.PotionEffect)));
   logAddPourSolvent(Infinity);
   straighten(degToRad(149), Salt.Sun, { preStirLength: 11.2, maxGrains: 184 });
   stirIntoVortex(3.5);
   console.log(radToDeg(getBottlePolarAngleByEntity()) + 180);
-  heatAndPourToEdge(0.5, 7);
-  logAddHeatVortex(3.4); // Empirical
+  heatAndPourToEdge(0.2, 14);
+  logAddHeatVortex(3.35); // Empirical
   console.log(getBottlePolarAngleByEntity());
   stirToTier(Effects.Water.WildGrowth, {
     preStirLength: 1.7,
     maxDeviation: DeviationT1,
     ignoreAngle: true,
   });
-  console.log(getBottlePolarAngleByEntity(EntityPotionEffect) + Math.PI);
+  console.log(getBottlePolarAngleByEntity(Entity.PotionEffect) + Math.PI);
   logAddPourSolvent(Infinity);
   // section 2
-  stirToTurn(3.25, Infinity, 0.1 * SaltAngle); // Empirical
+  stirToTurn(3.25, Infinity, { directionBuffer: 0.0 }); // Empirical
   straighten(degToRad(-45), Salt.Sun, { maxStirLength: 0.85, maxGrains: 250 });
   stirIntoVortex();
   logAddStirCauldron(0.13); // to not die.
   console.log(radToDeg(getBottlePolarAngleByEntity()) - 180);
   logAddHeatVortex(5.8);
   stirToTurn(7);
+  straighten(degToRad(-73), Salt.Sun, { maxGrains: 190 });
   stirIntoVortex();
+  console.log(radToDeg(getBottlePolarAngleByEntity()) - 180);
   logAddHeatVortex(Infinity);
   stirIntoVortex();
   logAddSunSalt(Math.ceil((180.01 + currentPlot.pendingPoints[0].angle) / 0.36));
@@ -63,7 +65,7 @@ function beta() {
   logAddHeatVortex(5.3);
   derotateToAngle(50, { toAngle: false });
   stirToNearestTarget(Effects.Water.Libido, { preStirLength: 5.7, maxStirLength: 0.3 });
-  logAddPourSolvent(2);
+  logAddPourSolvent(1.5);
   logAddStirCauldron(4);
   logAddPourSolvent(5);
   logAddHeatVortex(Infinity);
