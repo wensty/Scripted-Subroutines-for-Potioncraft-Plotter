2e0186717a20d6143886d6dd20a48265aa4bde67
W <986083475@qq.com>
2025-10-27 22:46:20 +0800

    Update virtual mode test.

--- test/test_virtual_subroutine.js
+++ test/test_virtual_subroutine.js
@@ -1,32 +1,33 @@
 import {
-  createAddIngredient,
-  createAddSunSalt,
-  createPourSolvent,
-  createStirCauldron,
+  logSkirt,
+  logAddSunSalt,
+  logAddStirCauldron,
+  logAddPourSolvent,
   stirIntoVortex,
+  setVirtual,
+  unsetVirtual,
 } from "../mainScript";
 
-import { Ingredients } from "@potionous/dataset";
-
-function virtualStirToVortex() {
-  let minTotalStir = Infinity;
+function testVirtualMode() {
+  logAddSunSalt(20);
+  logSkirt();
   let optimalStir;
-  let d = 0;
-  for (d = 5; d < 5.5; d += 0.01) {
-    const d2 = stirIntoVortex(5.0, {
-      recipeItems: [
-        createAddSunSalt(20),
-        createAddIngredient(Ingredients.PhantomSkirt, 1.0),
-        createStirCauldron(d),
-        createPourSolvent(Infinity),
-      ],
-      virtual: true,
-    }).distance;
-    if (d + d2 < minTotalStir) {
-      minTotalStir = d + d2;
-      optimalStir = d2;
+  let optimalTotalStir = Infinity;
+  for (let d = 5.0; d < 5.5; d += 0.01) {
+    setVirtual();
+    logAddStirCauldron(d);
+    logAddPourSolvent(Infinity);
+    const totalStir = stirIntoVortex(5).distance + d;
+    if (totalStir < optimalTotalStir) {
+      optimalStir = d;
+      optimalTotalStir = totalStir;
     }
   }
-
+  // console.log(stirIntoVortex(3))
+  console.log(optimalStir);
+  unsetVirtual();
+  logAddStirCauldron(optimalStir);
+  logAddPourSolvent(Infinity);
+  stirIntoVortex(5);
   console.log(optimalStir);
 }
