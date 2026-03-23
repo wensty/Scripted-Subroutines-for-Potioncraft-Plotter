3fe09ca8409fda860101dad805ecf5e2284960e4
W <986083475@qq.com>
2026-03-23 09:20:04 +0800

    Updated.

--- recipes/LightVision(SST2).js
+++ recipes/LightVision(SST2).js
@@ -1,16 +1,23 @@
 import {
   logAddIngredient,
   logAddIngredients,
+  logSkirt,
   logAddSunSalt,
   logAddHeatVortex,
   logAddStirCauldron,
   logAddPourSolvent,
   stirIntoVortex,
+  stirToTurn,
+  stirToDangerZoneExit,
   stirToTarget,
   stirToConsume,
+  pourToVortexEdge,
+  heatAndPourToEdge,
   derotateToAngle,
+  pourUntilAngle,
   degToRad,
   radToDeg,
+  vecToDir,
   vecToDirCoord,
   getAngleEntity,
   getStirDirection,
@@ -19,10 +26,13 @@ import {
   getDeviation,
   getCoord,
   straighten,
+  vSub,
   getSun,
-  BaseNames,
+  getStir,
+  setVirtual,
+  unsetVirtual,
 } from "../mainScript";
-import { SaltNames, Effects } from "../mainScript";
+import { SaltNames, BaseNames, Effects } from "../mainScript";
 
 import { Ingredients } from "@potionous/dataset";
 
@@ -38,6 +48,16 @@ const recipes = {
     Salts: { SunSalt: 278 },
     Effects: SST2,
     script: () => r1(),
+  },
+  r2: {
+    title: "SST2",
+    desc: "Ing-optimized",
+    version: "BetaV3",
+    base: BaseNames.Water,
+    Ingredients: { PhantomSkirt: 2 },
+    Salts: { SunSalt: 497 },
+    Effects: SST2,
+    script: () => r2(),
   },
 };
 
@@ -81,9 +101,7 @@ function r1() {
   logAddPourSolvent(0.8);
   logAddIngredient(Ingredients.Lifeleaf);
   logAddSunSalt(12);
-  console.log(
-    "distance: " + stirToTarget(Effects.Water.Light, { preStir: 1.0, maxStir: 1.0 }).distance
-  );
+  console.log("distance: " + stirToTarget(Effects.Water.Light, { preStir: 1.0, maxStir: 1.0 }).distance);
   console.log(getDeviation(Effects.Water.Light).total);
   logAddSunSalt(t1 + t2 - getSun());
   logAddIngredient(Ingredients.Lifeleaf);
@@ -92,4 +110,67 @@ function r1() {
   logAddPourSolvent(Infinity);
   stirIntoVortex(19);
   logAddHeatVortex(6.356);
+}
+
+function r2() {
+  const pre = 13;
+  const target1 = 195;
+  logAddSunSalt(pre);
+  logSkirt();
+  stirToTurn({ preStir: 5.33, directionBuffer: 0 });
+  logAddPourSolvent(Infinity);
+  const d1 = getStirDirection();
+  console.log("d1: " + radToDeg(d1));
+  stirIntoVortex(5);
+  console.log("~d1: " + (radToDeg(getAngleEntity()) - 90));
+  logAddHeatVortex(4.5);
+  logAddPourSolvent(1.31);
+  logAddHeatVortex(3.785);
+  const d2 = getHeatDirection();
+  console.log("d2: " + radToDeg(d2));
+  straighten(d2 - Math.PI / 2, SaltNames.Sun, { preStir: 3, maxGrains: target1 - pre });
+  stirIntoVortex(4);
+  console.log("~d2: " + (radToDeg(getAngleEntity()) - 90));
+
+  // Aligning + Return
+
+  derotateToAngle(43.8);
+  logAddHeatVortex(Infinity);
+  stirIntoVortex(2);
+  logAddHeatVortex(Infinity);
+  logAddPourSolvent(0.53);
+  setVirtual();
+  stirToTarget(Effects.Water.Light, { maxStir: 3 });
+  console.log("deviation<600%: " + getDeviation(Effects.Water.Light).total);
+  unsetVirtual();
+  logAddStirCauldron(2.98);
+  pourUntilAngle(97 * 0.36);
+  console.log("total stir:" + getStir());
+  logSkirt();
+
+  // part 2
+
+  logAddPourSolvent(Infinity);
+  logAddSunSalt(99);
+  stirToTurn();
+  const c1 = getCoord();
+  console.log(vecToDir(c1));
+  logAddSunSalt(57);
+  logAddStirCauldron(4.55);
+  const c2 = getCoord();
+  console.log(vecToDir(vSub(c2, c1)));
+  console.log(getStirDirection());
+  straighten(degToRad(115), SaltNames.Sun, { maxGrains: target1 + 302 - getSun() });
+
+  // MagicVision
+
+  stirToTurn({ preStir: 12.2 });
+  logAddHeatVortex(3);
+  stirToConsume(10.26);
+  derotateToAngle(0);
+  pourToVortexEdge();
+  heatAndPourToEdge(1, 11);
+  logAddHeatVortex(2.51);
+  stirToDangerZoneExit(2);
+  logAddPourSolvent(0.11);
 }
